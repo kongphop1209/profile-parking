ca99c6df3b21ff16798a109d53f0774492d83b12
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-11-21 12:21:02 +0700

    poreparing pj

---
+++ profile/src/app/(pages)/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Github, Linkedin, Twitter } from "lucide-react";
+
+const main = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-4">
+      main
+    </div>
+  )
+}
+
+export default main
---
+++ profile/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Main from "@/app/(pages)/main"
+
+export default function Home() {
+  return (
+    <div>
+      <Main/>
+    </div>
+  );
+}
