92fa1cf71159af230064a1f36425cfd555b17365
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-11-24 21:14:45 +0700

    re create project

--- profile/tailwind.config.ts
+++ profile/tailwind.config.ts
@@ -5,11 +5,12 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
