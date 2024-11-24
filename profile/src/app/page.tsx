48bae926814816e8c8e8b6ada65657e6327ead59
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-11-24 21:24:59 +0700

    a

--- profile/src/app/page.tsx
+++ profile/src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import React from 'react';
+import Navbar from "./components/navbar";
+import Header from "./components/header";
 
 export default function Home() {
   return (
-    <div>
-      aa
+    <div className='w-full'>
+      <Header/>
+      <Navbar/>
+      <div>lorem*30</div>
     </div>
   );
 }
