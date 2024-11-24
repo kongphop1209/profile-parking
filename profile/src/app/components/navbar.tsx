9697487d134345e7736b1ca83f5d1f55a81c628e
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-11-24 20:58:16 +0700

    a

--- profile/src/app/components/navbar.tsx
+++ profile/src/app/components/navbar.tsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div className='bg-black w-screen'>
-        <ul className=''>
-          <li>Home</li>
-          <li>About Me</li>
-          <li>Skills</li>
-          <li>Projects</li>
-          <li>Experience</li>
-          <li>Contact</li>
+    <nav className="bg-black w-screen fixed top-0 z-50">
+      <div className="w-full">
+        {/* Navigation Links */}
+        <ul className="flex flex-row justify-center items-center space-x-6 text-white py-4">
+          <li>
+            <a href="#home" className="hover:text-gray-400 transition">Home</a>
+          </li>
+          <li>
+            <a href="#about" className="hover:text-gray-400 transition">About Me</a>
+          </li>
+          <li>
+            <a href="#skills" className="hover:text-gray-400 transition">Skills</a>
+          </li>
+          <li>
+            <a href="#projects" className="hover:text-gray-400 transition">Projects</a>
+          </li>
+          <li>
+            <a href="#experience" className="hover:text-gray-400 transition">Experience</a>
+          </li>
+          <li>
+            <a href="#contact" className="hover:text-gray-400 transition">Contact</a>
+          </li>
         </ul>
-    </div>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default navbar
+export default Navbar;
