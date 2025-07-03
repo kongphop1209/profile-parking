c26d52dc3c4982c2b57d5728b69030de78d54e2f
parking <6531503008@lamduan.mfu.ac.th>
2025-07-04 00:50:12 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const Index = dynamic(() => import('@/app/pages/index'), { ssr: false });
 export default function Home() {
   return (
     <main>
-    <Index/>
+      <Index/>
     </main>
   );
 }
